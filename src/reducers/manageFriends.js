2a53b11c8651080541f24fdcd64f46e6e524bbb3
Rudy Ravelin <rudy@noukod.com>
2019-04-11 14:28:32 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -5,14 +5,14 @@ export function manageFriends(state, action){
         friends: [...state.friends, {name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id}]
       };
 
-// case 'REMOVE_FRIEND':
-//       const removeIndex = state.friends.findIndex(friend => friend.id === action.id);
-//       return {
-//         ...state,
-//         friends: [...state.friends.slice(0, removeIndex), ...state.friends.slice(removeIndex + 1)]
-//       };
-//
-//     default:
-//       return state;
-//   }
-// }
+case 'REMOVE_FRIEND':
+      const removeIndex = state.friends.findIndex(friend => friend.id === action.id);
+      return {
+        ...state,
+        friends: [...state.friends.slice(0, removeIndex), ...state.friends.slice(removeIndex + 1)]
+      };
+
+    default:
+      return state;
+  }
+}
